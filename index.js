d19bdde55efe0926ec6aa558c83ea2bac57b0b90
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-01-11 14:44:44 -0800

    second test passing

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function distanceFromHqInBlocks(currentLocation) {
   let distanceInBlocks = Math.abs(currentLocation - 42);
   return distanceInBlocks;
 }
+function distanceFromHqInFeet(blocks) {
+  let feetDistance = distanceFromHqInBlocks(blocks)
+  return feetDistance * 264 
+}
+//can we or how could we refactor the above function into an arrow function
